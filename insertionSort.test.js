ac9e3791a21078ca51ba712b879c2e105e59e77b
leechongren <lee_chongren@hotmail.com>
2020-04-17 15:50:00 +0800

    add wider test

--- insertionSort.test.js
+++ insertionSort.test.js
@@ -12,4 +12,12 @@ describe("insertionSort", () => {
   it("should return [1,2,3] when input [3,2,1]", () => {
     expect(insertionSort([3, 2, 1])).toEqual([1, 2, 3]);
   });
+
+  it("should return [1,2,3,4] when input [3,2,4,1]", () => {
+    expect(insertionSort([3, 2, 4, 1])).toEqual([1, 2, 3, 4]);
+  });
+
+  it("should return [2,3,4,6,8] when input [4,2,3,8,6]", () => {
+    expect(insertionSort([4, 2, 3, 8, 6])).toEqual([2, 3, 4, 6, 8]);
+  });
 });
