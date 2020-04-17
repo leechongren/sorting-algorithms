b905be56e5bf6d0c65fb485f75b9c0236ece0407
leechongren <lee_chongren@hotmail.com>
2020-04-17 20:23:39 +0800

    change testcode to describe individidual testing

--- insertionSort.test.js
+++ insertionSort.test.js
@@ -1,27 +1,27 @@
 const insertionSort = require("./insertionSort");
 
 describe("insertionSort", () => {
+  it("should return empty array if array is empty", () => {
+    expect(insertionSort([])).toEqual([]);
+  });
+
   it("should return [1] when input [1]", () => {
     expect(insertionSort([1])).toEqual([1]);
   });
 
-  it("should return [1,2] when input [2,1]", () => {
-    expect(insertionSort([2, 1])).toEqual([1, 2]);
+  it("should return sorted array if array is not sorted", () => {
+    expect(insertionSort([1, 10, 3, 5])).toEqual([1, 3, 5, 10]);
   });
 
-  it("should return [1,2,3] when input [3,2,1]", () => {
-    expect(insertionSort([3, 2, 1])).toEqual([1, 2, 3]);
+  it("should return sorted array if array is reversely sorted", () => {
+    expect(insertionSort([10, 5, 3, 1])).toEqual([1, 3, 5, 10]);
   });
 
-  it("should return [1,2,3,4] when input [3,2,4,1]", () => {
-    expect(insertionSort([3, 2, 4, 1])).toEqual([1, 2, 3, 4]);
+  it("should return sorted array if array is not sorted with negative numbers", () => {
+    expect(insertionSort([1, -10, 3, 5])).toEqual([-10, 1, 3, 5]);
   });
 
-  it("should return [2,3,4,6,8] when input [4,2,3,8,6]", () => {
-    expect(insertionSort([4, 2, 3, 8, 6])).toEqual([2, 3, 4, 6, 8]);
-  });
-
-  it("should return [2,3,3,4,6,8] when input [4,2,3,8,6,3]", () => {
-    expect(insertionSort([4, 2, 3, 8, 6, 3])).toEqual([2, 3, 3, 4, 6, 8]);
+  it("should return sorted array if has duplicates", () => {
+    expect(insertionSort([2, 1, 1, 3, 5])).toEqual([1, 1, 2, 3, 5]);
   });
 });
